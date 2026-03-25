d9ba23731d6c0cb155c6f0a27068d0476bbe09f4
maxx6618 <maximilian.goeppert@nextgen-equity.com>
2026-03-25 08:40:01 -0600

    Initial Briefr click dummy — Vite + React setup

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BriefrApp from './BriefrApp.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BriefrApp />
+  </React.StrictMode>
+)
